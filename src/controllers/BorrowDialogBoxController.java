33b2fcf77b4ba58dbaa98b1dbcad36aa7f4bd942
WenHao1223 <wenhaojshs@gmail.com>
2024-11-24 06:52:30 +0800

    Implement borrow book

--- src/controllers/BorrowDialogBoxController.java
+++ src/controllers/BorrowDialogBoxController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Optional;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -13,12 +14,22 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import models.Book;
 
 public class BorrowDialogBoxController {
   private Library library;
   private Book book;
+
+  @FXML
+  private TextField tfBorrowerName;
+
+  @FXML
+  private TextField tfBorrowerPhone;
+
+  @FXML
+  private TextField tfBorrowerID;
 
   public void setLibrary(Library library) {
     this.library = library;
@@ -55,6 +66,37 @@ public class BorrowDialogBoxController {
       }
     } else {
       // User chose CANCEL or closed the dialog
+      return;
+    }
+  }
+
+  public void handleBorrowButtonAction(ActionEvent event) {
+    Alert alert = new Alert(AlertType.CONFIRMATION);
+    alert.setTitle("Borrow Book");
+    alert.setHeaderText(null);
+    alert.setContentText("Are you sure you want to borrow this book?");
+    Optional<ButtonType> action = alert.showAndWait();
+
+    if (action.isPresent() && action.get() == ButtonType.OK) {
+      try {
+        String borrowerName = tfBorrowerName.getText();
+        String borrowerPhone = tfBorrowerPhone.getText();
+        long borrowerID = Long.parseLong(tfBorrowerID.getText());
+
+        book.borrowBook(borrowerName, borrowerPhone, borrowerID);
+
+        // Show a confirmation alert box
+        Alert alert2 = new Alert(AlertType.INFORMATION);
+        alert2.setTitle("Borrow Book");
+        alert2.setHeaderText(null);
+        alert2.setContentText("Book borrowed successfully!");
+        alert2.showAndWait();
+
+        ((Node) (event.getSource())).getScene().getWindow().hide();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    } else {
       return;
     }
   }
