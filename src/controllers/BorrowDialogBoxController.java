8b0742085654cb8552d31d41c732cdeb65d516cd
WenHao1223 <wenhaojshs@gmail.com>
2024-11-24 06:18:01 +0800

    Link ViewBookScene with BorrowDialogox

--- src/controllers/BorrowDialogBoxController.java
+++ src/controllers/BorrowDialogBoxController.java
@@ -1,11 +1,28 @@
 package controllers;
 
 import models.Library;
+import models.Book;
 
 public class BorrowDialogBoxController {
   private Library library;
+  private Book book;
 
   public void setLibrary(Library library) {
     this.library = library;
+  }
+
+  public void setBookDetails(String isbn) {
+    if (library != null) {
+      Book book = library.getBookByISBN(isbn);
+      if (book != null) {
+        this.book = book;
+      } else {
+        System.err.println("Book not found for ISBN: " + isbn);
+      }
+    } else {
+      System.err.println("Library is null in BorrowDialogBoxController");
+    }
+
+    book.displayBookDetails();
   }
 }
--- src/controllers/ViewBookSceneController.java
+++ src/controllers/ViewBookSceneController.java
@@ -9,15 +9,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class ViewBookSceneController {
   private Library library;
+  private Book book = new Book();
 
   @FXML
   private Label bookTitleLabel;
@@ -32,6 +40,9 @@ public class ViewBookSceneController {
   private Label isAvailableLabel;
 
   @FXML
+  private Button btnBorrowOrReturn;
+
+  @FXML
   private ImageView bookImageView;
 
   public void setLibrary(Library library) {
@@ -40,16 +51,22 @@ public class ViewBookSceneController {
 
   public void setBookDetails(String isbn) {
     System.out.println(isbn);
-    Book book = library.getBookByISBN(isbn);
-    if (book != null) {
-      bookTitleLabel.setText(book.getTitle());
-      authorNameLabel.setText(book.getAuthor());
-      isbnLabel.setText(book.getISBN());
-      isAvailableLabel.setText(book.getAvailability() ? "Available" : "Borrowed by " + book.getBorrowerName());
+    this.book = library.getBookByISBN(isbn);
+
+    if (this.book != null) {
+      bookTitleLabel.setText(this.book.getTitle());
+      authorNameLabel.setText(this.book.getAuthor());
+      isbnLabel.setText(this.book.getISBN());
+      isAvailableLabel
+          .setText(this.book.getAvailability() ? "Available" : "Borrowed by " + this.book.getBorrowerName());
     } else {
       System.err.println("Book is null in setBookDetails");
     }
     setImage(isbn);
+
+    if (!this.book.getAvailability()) {
+      btnBorrowOrReturn.setText("Return");
+    }
   }
 
   private void setImage(String isbn) {
@@ -67,6 +84,41 @@ public class ViewBookSceneController {
       System.err.println("Error loading image: " + e.getMessage());
       e.printStackTrace();
     }
+  }
+
+  @FXML
+  private void handleBorrowOrReturnButtonAction(ActionEvent event) {
+    if (this.book.getAvailability()) {
+      System.out.println("Borrowing book...");
+      Alert alert = new Alert(AlertType.CONFIRMATION);
+      alert.setTitle("Borrow Book");
+      alert.setHeaderText(null);
+      alert.setContentText("Are you sure you want to borrow this book?");
+      Optional<ButtonType> action = alert.showAndWait();
+
+      if (action.get() == ButtonType.OK) {
+        try {
+          FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/components/BorrowDialogBox.fxml"));
+          Parent root = fxmlLoader.load();
+          BorrowDialogBoxController borrowController = fxmlLoader.getController();
+          borrowController.setLibrary(library);
+          borrowController.setBookDetails(this.book.getISBN());
+
+          Stage dialogStage = new Stage();
+          dialogStage.setTitle("Borrow Book");
+          dialogStage.initModality(Modality.WINDOW_MODAL);
+          dialogStage.initOwner(((Node) event.getSource()).getScene().getWindow());
+          dialogStage.setResizable(false);
+          dialogStage.setScene(new Scene(root));
+          dialogStage.showAndWait();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    } else {
+      System.out.println("Returning book...");
+    }
+    setBookDetails(this.book.getISBN());
   }
 
   @FXML
